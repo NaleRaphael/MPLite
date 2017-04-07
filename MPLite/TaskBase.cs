fb9f55d89ba3ca5725904c3d9606c8060a8087ba
NaleRaphael <gmccntwxy@gmail.com>
2017-04-07 11:06:19 +0800

    UPDATE: bg-color of selectedUnfocused listboxitem

---
+++ MPLite/TaskBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MPLite
+{
+    public abstract class TaskBase
+    {
+        public int Rank { get; set; }
+        public bool Enabled { get; set; }
+        
+        public bool ThisDayForwardOnly { get; set; }
+    }
+
+    public class DailyTask : TaskBase
+    {
+
+    }
+
+    public class TaskGroup
+    {
+        // Multiple daily tasks
+    }
+}
