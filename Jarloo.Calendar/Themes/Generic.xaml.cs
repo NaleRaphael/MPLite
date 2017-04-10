304619201db0a6f1758186a9e2183a3ba71f0ab8
NaleRaphael <gmccntwxy@gmail.com>
2017-04-10 11:25:19 +0800

    UPDATE: get date from clicked DockPanel

--- Jarloo.Calendar/Themes/Generic.xaml.cs
+++ Jarloo.Calendar/Themes/Generic.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace Jarloo.Calendar.Themes
@@ -20,6 +21,16 @@ namespace Jarloo.Calendar.Themes
             if (e.RightButton == MouseButtonState.Pressed && e.LeftButton == MouseButtonState.Released)
             {
                 MessageBox.Show("ListBox: Say YO");
+            }
+            return;
+        }
+
+        private void DockPanel_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.RightButton == MouseButtonState.Pressed && e.LeftButton == MouseButtonState.Released)
+            {
+                Day info = (Day)((DockPanel)sender).DataContext;
+                MessageBox.Show(info.Date.ToString());
             }
             return;
         }
