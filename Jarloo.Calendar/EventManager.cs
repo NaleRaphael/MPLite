c4fb8c04302ce72825f7c1b6d86265ec8b13d210
NaleRaphael <gmccntwxy@gmail.com>
2017-04-11 14:31:48 +0800

    UPDATE: implement refreshTimer for EventManager

--- Jarloo.Calendar/EventManager.cs
+++ Jarloo.Calendar/EventManager.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Windows.Threading;
 
 namespace Jarloo.Calendar
 {
     public class EventManager
     {
+        private DispatcherTimer refreshTimer;
+
         // EventManger only have to handle events in one day.
         // Update event list when day is changed.
         public ICollection<IEvent> EventList;
@@ -17,6 +17,7 @@ namespace Jarloo.Calendar
         {
             EventList = new List<IEvent>();
             TimerList = new List<DispatcherTimer>();
+            InitRefreshTimer();
         }
 
         // Before execute `AddEvent`, manager should check whether the event is in range (one day).
@@ -29,15 +30,39 @@ namespace Jarloo.Calendar
             TimerList.Add(evnt.Timer);
         }
 
-        // Refresh tasks when this app is opened, or the day is changed. (e.g. 2017/04/10 -> 2017/04/11)
-        public void RefreshTasks()
-        {
-            // NOTE: check whether there will be some unfinished tasks being disposed?
-        }
-
         private void CheckEventIsInRange(IEvent evnt)
         {
 
         }
+
+        #region refreshTimer control
+        private void InitRefreshTimer()
+        {
+            if (refreshTimer != null)
+                return;
+
+            // refreshTimer will be triggered at 0:00 of a day
+            refreshTimer = new DispatcherTimer { Interval = DateTime.Today.AddDays(1) - DateTime.Now };
+            refreshTimer.Tick += (sender, args) =>
+            {
+                refreshTimer.Stop();
+
+                // Update task lists
+                RefreshTasks();
+
+                // Reset refreshTimer
+                refreshTimer = null;
+                InitRefreshTimer();
+            };
+            refreshTimer.Start();
+        }
+
+        // Refresh tasks when this app is opened, or the day is changed. (e.g. 2017/04/10 -> 2017/04/11)
+        public void RefreshTasks()
+        {
+            // NOTE: check whether there will be some unfinished tasks being disposed?
+            return;
+        }
+        #endregion
     }
 }
